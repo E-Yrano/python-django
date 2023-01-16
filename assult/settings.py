cebfbba1ca9bde7db28612a8b80fd90f428ebe9d
SoonHwanKwon <101018462+E-Yrano@users.noreply.github.com>
2023-01-16 12:55:53 +0900

    Update settings.py

--- assult/settings.py
+++ assult/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 # import os, json
 from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
