7fad55a894b3bd5224b047e1847e6e3526698019
SoonHwanKwon <101018462+E-Yrano@users.noreply.github.com>
2023-01-20 18:55:58 +0900

    Update settings.py

--- assult/settings.py
+++ assult/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECRET_KEY = get_secret("SECRET_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
