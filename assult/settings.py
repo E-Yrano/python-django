b5c2411fbd707b394eed9db99adcfa7d72f92abc
SoonHwanKwon <101018462+E-Yrano@users.noreply.github.com>
2023-01-27 20:01:41 +0900

    Update settings.py

--- assult/settings.py
+++ assult/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'dbtest.apps.DbtestConfig',
-    'myapp.apps.MyappConfig',
+#     'myapp.apps.MyappConfig',
 #     'bootstrap4',
     'myapp'
 ]
