a0389bd6870d10a1c7ca47fed95523b1ce9841c9
SoonHwanKwon <101018462+E-Yrano@users.noreply.github.com>
2023-01-31 13:50:47 +0900

    Update settings.py

--- assult/settings.py
+++ assult/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static'),
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
