6407bf716d5b2d63b66cd2ae6a634cb039f40564
SoonHwanKwon <101018462+E-Yrano@users.noreply.github.com>
2023-02-06 19:39:31 +0900

    Update settings.py

--- assult/settings.py
+++ assult/settings.py
@@ -134,12 +134,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+# #     os.path.join(BASE_DIR, '/static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '/static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-# Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
